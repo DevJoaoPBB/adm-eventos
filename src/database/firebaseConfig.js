7ddea3fd3236b8fd46ff4a2cb2675c14a8daee41
João Pedro Bagnara Boeing <joaopedroboeing688@gmail.com>
2024-04-26 20:26:41 -0300

    Update firebaseConfig.js

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,13 +1,15 @@
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAbdsN4VlWRzL0lSEPWb2ibkvknX3zV7rI",
-    authDomain: "ronan-admin-evento.firebaseapp.com",
-    projectId: "ronan-admin-evento",
-    storageBucket: "ronan-admin-evento.appspot.com",
-    messagingSenderId: "345124784036",
-    appId: "1:345124784036:web:aeafbe7f139b649a64e331"
+  apiKey: "AIzaSyABsJeYRaSYMDYxB4EJ4rxQPUE8In4C4Yo",
+  authDomain: "admeventos-7c02a.firebaseapp.com",
+  projectId: "admeventos-7c02a",
+  storageBucket: "admeventos-7c02a.appspot.com",
+  messagingSenderId: "816176535044",
+  appId: "1:816176535044:web:2b7113441003519e12c751"
 };
 
 const app = initializeApp(firebaseConfig);
